ba0a26a699b7b52fcbbc07d1cc17ab87ec79590b
Brian Sanders <bdougsand+cfb@gmail.com>
2015-05-12 11:06:36 -0400

    Minor improvements to calendar view
    - Show the name of the selected task at the top
    - Show summary of tasks performed on a day it's clicked

--- src/whats_next/calendar.cljs
+++ src/whats_next/calendar.cljs
@@ -7,41 +7,58 @@
 
 (def day-count -10)
 
+(defn day-summary-view [work owner]
+  (reify
+    om/IRender
+    (render [_]
+      (let [duration (state/total-duration work)]
+        (dom/div #js {:className "task-overview"}
+                 (count work)
+                 " totaling "
+                 ($/pretty-duration duration))))))
+
 (defn calendar-view [app owner]
   (reify
     om/IRenderState
-    (render-state [_ {:keys [task-type end-date]}]
-      (dom/div
-       #js {:className "calendar-container"}
-       (dom/div
-        #js {:className "calendar"}
+    (render-state [_ {:keys [end-date expanded task-type]}]
+      (let [month (.getMonth end-date)
+            start-date ($/start-of-week ($/inc-date end-date day-count))
+            work-by-date (state/day-groups
+                          (sequence
+                           (comp (state/between (.getTime start-date)
+                                                (.getTime end-date))
+                                 (state/type-filter task-type))
+                           (:work app)))]
         (dom/div
+         #js {:className "calendar-container"}
+         (dom/h3 nil task-type)
+         (dom/div
+          #js {:className "calendar"}
+          (dom/div
            #js {:className "col-labels"}
            (for [week-day $/short-day-names]
              (dom/div #js {:className "day-col"} week-day)))
-        (let [month (.getMonth end-date)
-              start-date ($/start-of-week ($/inc-date end-date day-count))
-              work-by-date (state/day-groups
-                            (sequence
-                             (comp (state/between (.getTime start-date)
-                                                  (.getTime end-date))
-                                   (state/type-filter task-type))
-                             (:work app)))]
           (for [week (partition-all 7 ($/date-range start-date end-date))]
             (dom/div #js {:className "week"}
                      (for [date week
-                           :let [work (work-by-date ($/day-components date))]]
-                       (dom/div #js {:className (str "day "
-                                                     (when (= (.getMonth date) month)
-                                                       "this-month ")
-                                                     (when work
-                                                       "worked"))}
-                                (dom/div #js {:className "date"}
-                                         (.getDate date))))))))
+                           :let [dc ($/day-components date)
+                                 work (work-by-date dc)]]
+                       (dom/a #js {:className (str "day "
+                                                   (when (= (.getMonth date) month)
+                                                     "this-month ")
+                                                   (when work
+                                                     "worked"))
+                                   :href "#"
+                                   :onClick #(om/set-state! owner :expanded dc)}
+                              (dom/div #js {:className "date"}
+                                       (.getDate date)))))))
 
-       (dom/div #js {:className "task-select"}
-                (apply dom/select
-                       #js {:onChange #(om/set-state! owner :task-type
-                                                      (.. % -target -value))}
-                       (for [{n :name} (:task-types app)]
-                         (dom/option #js {:value n} n))))))))
+         (dom/div #js {:className "task-select"}
+                  (apply dom/select
+                         #js {:onChange #(om/set-state! owner :task-type
+                                                        (.. % -target -value))}
+                         (for [{n :name} (:task-types app)]
+                           (dom/option #js {:value n} n))))
+
+         (when expanded
+           (om/build day-summary-view (work-by-date expanded))))))))
